8899672f2c95427fdc4c9a92766fa5547a945c6e
FarzadVav <vav.progg.ir@gmail.com>
2023-11-16 05:30:06 -0800

    complete handel loaders

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
+import { LoaderButtonContextProvider } from "./contexts/LoaderButtonContext"
 import LoaderButton from "./components/LoaderButton"
 
 const App = () => {
+  const postRequest = (cancelLoading: Function) => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .finally(() => cancelLoading())
+  }
+
   return (
-    <>
-      <LoaderButton>
+    <LoaderButtonContextProvider>
+      <LoaderButton clickHandler={cancelLoading => postRequest(cancelLoading)}>
         SUBMIT 1
       </LoaderButton>
-      <LoaderButton>
+      <LoaderButton clickHandler={cancelLoading => postRequest(cancelLoading)}>
         SUBMIT 2
       </LoaderButton>
-      <LoaderButton>
+      <LoaderButton clickHandler={cancelLoading => postRequest(cancelLoading)}>
         SUBMIT 3
       </LoaderButton>
-    </>
+    </LoaderButtonContextProvider>
   )
 }
 
--- src/components/LoaderButton.tsx
+++ src/components/LoaderButton.tsx
@@ -1,13 +1,41 @@
-import { ReactNode } from "react"
+import { ReactNode, useContext, useState, useEffect } from "react"
+import { v4 as uuid } from 'uuid'
+import { PulseLoader } from "react-spinners";
+
+import LoaderButtonContext from "../contexts/LoaderButtonContext";
 
 type LoaderButtonProps = {
+  className?: string,
+  clickHandler: (cancelLoading: () => void, event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void,
   children: ReactNode
 }
 
-const LoaderButton = ({ children }: LoaderButtonProps) => {
+const LoaderButton = ({ className, clickHandler, children }: LoaderButtonProps) => {
+  const loader = useContext(LoaderButtonContext)
+  const [primaryKey, setPrimaryKey] = useState<string>('')
+
+  useEffect(() => {
+    setPrimaryKey(uuid())
+  }, [])
+
+  const cancelLoading = () => loader.removeLoadingHandler(primaryKey)
+
   return (
-    <button>
-      {children}
+    <button
+      id={primaryKey}
+      className={className}
+      onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        loader.addLoadingHandler(primaryKey)
+        clickHandler(cancelLoading, event)
+      }}
+      type={'submit'}
+    >
+      {
+        (loader.loading
+          && loader.loading.primaryKeys.find(key => key === primaryKey))
+          ? <div><PulseLoader color='black' size={6} /></div>
+          : children
+      }
     </button>
   )
 }
